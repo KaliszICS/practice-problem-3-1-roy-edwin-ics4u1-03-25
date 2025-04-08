70b06247a2486d8c8e7c787bb081e0b08ea75487
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 08:59:27 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,25 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
-
 	
-
+public static int sumOfDigits(int x) {
+        if (x < 0) {
+        x = x * -1;
+        }
+        if (x < 10) {
+        return x;
+        }
+return (x % 10) + sumOfDigits(x / 10);
+    }
+	
+public static int factorial(int n) {
+        if (n < 0) {
+        return 0;
+        }
+        if (n == 0) {
+        return 1;
+        }
+return n * factorial(n - 1);
+	}
 }
